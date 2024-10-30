f2f14ef1c0e8b15d486579c45977ad74b27d22c3
reamted <nroussos2028@kehillahstudent.org>
2024-10-30 12:06:35 -0700

    asighment + fizzbuzz

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ When called, your function should print out ALL of the multiples of `n` between 
 
 Call your function for `n = 2`, `n = 3`, `n = 5`, and `n = 12`.
 '''
+
+
+
+
+#actually asighment
+for e in range(1, 100):
+    n=e
+    for i in range(101):
+        x = i
+        if x % n == 0:
+            print(i)
+
+
+#fizzbuzz 
+for n in range(1, 100):
+    if n % 15 ==0:
+        print("fizzbuzz")
+    if n % 5 ==0:
+        print("fizz")
+    if n % 3 ==0:
+        print("buzz")
+    
